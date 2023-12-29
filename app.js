2313465d93c948e988d722e5b8587bd092c64e35
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2023-12-29 16:59:00 +0100

    added an image

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images");
+    cb(null, "images");  
   },
 
   filename: (req, file, cb) => {
